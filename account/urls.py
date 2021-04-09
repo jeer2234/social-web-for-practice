f01a0ddbeea9804ee58cc25995d55ab7ffa495b8
Javier Espinoza <jeer2234@gmail.com>
2021-04-08 22:34:59 -0400

    implementacion parcial de registros

--- account/urls.py
+++ account/urls.py
@@ -6,8 +6,8 @@ app_name : 'account'
 urlpatterns = [
     # post views
     #path('login/', views.user_login, name='login'),
-   # path('login/', auth_views.LoginView.as_view(), name='login'),
-  #  path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    #  path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
     # change password urls
    # path('password_change/',auth_views.PasswordChangeView.as_view(),name='password_change'),
@@ -18,4 +18,6 @@ urlpatterns = [
    # path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
    # path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
     path('', include('django.contrib.auth.urls')),
-] 
+    path('register/', views.register, name='register'),
+  ] 
+
