f76fd6590e81ebf4b071242d87b92a75f5f29864
Javier Espinoza <jeer2234@gmail.com>
2021-04-09 14:55:01 -0400

    forms y views para el registro de pagina

--- account/forms.py
+++ account/forms.py
@@ -1,20 +1,32 @@
 from django import forms
 from django.contrib.auth.models import User
-
+from django.contrib.auth.password_validation import validate_password
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
 class UserRegistrationForm(forms.ModelForm):
+    
     password = forms.CharField(label='Password',widget=forms.PasswordInput)
+    
     password2 = forms.CharField(label='Repeat password',widget=forms.PasswordInput)
     
     class Meta:
         model = User
         fields = ('username', 'first_name', 'email')
+   # def clean_password(self):
+
+   #     password = self.cleaned_data.get('password')
+   #     try:                                                     analizar mas a fondo este codigo
+    #        validate_password(password, self.instance)
+    #    except forms.ValidationError as error:
+#
+ #           # Method inherited from BaseForm
+  #          self.add_error('password', error)
+   #     return password
         
-        def clean_password2(self):
-            cd = self.cleaned_data
-            if cd['password'] != cd['password2']:
-                raise forms.ValidationError('Passwords don\'t match.')
-            return cd['password2']
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm
+from django.contrib.auth.password_validation import validate_password
 
 def user_login(request):
     if request.method == 'POST':
@@ -38,7 +39,7 @@ def register(request):
         # Save the User object
             new_user.save()
             return render(request,'account/register_done.html',{'new_user': new_user})
-        else:
-            user_form = UserRegistrationForm()
-            return render(request,'account/register.html',{'user_form': user_form})
+    else:
+        user_form = UserRegistrationForm()
+        return render(request,'account/register.html',{'user_form': user_form})
 
