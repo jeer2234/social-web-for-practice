6770c3f1ee47269b29f28cb9f32c38b9f009db3e
Javier Espinoza <jeer2234@gmail.com>
2021-04-14 00:50:52 -0400

    implementacion perfil de usuario, capacidad de django de servir static files en modo debug

--- account/forms.py
+++ account/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
+from .models import Profile
+
+
 class LoginForm(forms.Form):
+
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
 
 class UserRegistrationForm(forms.ModelForm):
     
@@ -30,3 +35,18 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
+
+class UserEditForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
+
+
