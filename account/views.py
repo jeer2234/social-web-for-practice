8924cf9518e9ae99538fd03599e736bac013a670
Javier Espinoza <jeer2234@gmail.com>
2021-04-14 17:36:39 -0400

    añadiendo metodo para iniciar sesion atraves de email

--- account/views.py
+++ account/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.password_validation import validate_password
 from .models import Profile
+from django.contrib import messages
+
 
 
 def user_login(request):
@@ -58,6 +60,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
